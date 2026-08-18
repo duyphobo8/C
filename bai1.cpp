d318bc1ec9442d421677ee381cd006fb27b1c2bd
duy <duyphobo8@gmail.com>
2026-08-18 15:18:16 +0700

    helo

---
+++ bai1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t; cin>>t;
+    while (t--) {
+        int x; cin>>x;
+        int y; cin>>y;
+        vector<int> a(x);
+        vector<int> b(y);
+        for (int i = 0; i < x; i++) {
+            cin>>a[i];
+        }
+        for (int i = 0; i < y; i++) {
+            cin>>b[i];
+        }
+
+    }
+}
